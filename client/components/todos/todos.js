d5700f9bac5c451b2fbbd9f62e0ceb5e43ea4dd7
Taylor Page <jonathantaylorpage@gmail.com>
2016-08-10 13:00:31 -0500

    (feat) add todos component

---
+++ client/components/todos/todos.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export default class Todos extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <div className="container-fluid">
+        <h1>Todos</h1>
+      </div>
+      )
+  }
+
+}
